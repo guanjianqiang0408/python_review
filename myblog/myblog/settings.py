f210126b2e8a317f7933c6c20efbcb47b66bcd6c
guanjianqiang <905685941@qq.com>
2022-11-01 21:27:24 +0800

    feat: 项目初始化配置

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-_!fo%)yxdve3w3fw91d4_&!6z)djpodikv_d(03o8i%gjqw#bi
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+# 配置域名访问权限——任何域名均可访问
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 添加自定义APP应用
+    "blog.apps.BlogConfig"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,8 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 配置模板目录
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+# 配置中文
+LANGUAGE_CODE = 'zh-hans'
+# 配置时区
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -116,9 +121,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+# 设置静态文件目录和名称
 STATIC_URL = '/static/'
+# 设置静态文件夹目录
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+
+# 设置文件、图片、上传后存放路径位置
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
