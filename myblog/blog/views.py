6efd57d248395d0b819c785f92831641b3e3c2de
guanjianqiang <905685941@qq.com>
2022-11-05 08:46:10 +0800

    feature: 视图代码优化

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -4,6 +4,13 @@ from django.http import HttpResponse
 from .models import Article, Category, Banner, Tag, Link
 
 
+def global_variable(request):
+    allcategory = Category.objects.all()
+    remen = Article.objects.filter(tui__id=2)[:6]
+    tags = Tag.objects.all()
+    return locals()
+
+
 # Create your views here.
 def hello(request):
     return HttpResponse("欢迎Django")
@@ -11,36 +18,19 @@ def hello(request):
 
 # 首页
 def index(request):
-    categorys = Category.objects.all()
     banners = Banner.objects.filter(is_active=True)[0:4]
     tuis = Article.objects.filter(tui__id=1)
     articles = Article.objects.all().order_by("-id")[0:10]
     hot_articles = Article.objects.all().order_by("views")[:10]
-    remens = Article.objects.filter(tui__id=2)
-    tags = Tag.objects.all()
     link = Link.objects.all()
-    # 构造上下文
-    context = {
-        "categorys": categorys,
-        "banners": banners,
-        "tuis": tuis,
-        "articles": articles,
-        "hot_articles": hot_articles,
-        "remens": remens,
-        "tags": tags,
-        "link": link,
-    }
-    # 传递上下文
-    return render(request, "index.html", context)
+
+    return render(request, "index.html", locals())
 
 
 # 列表
 def list(request, lid):
     list = Article.objects.filter(category_id=lid)
     cname = Category.objects.get(id=lid)
-    remen = Article.objects.filter(tui__id=2)[:6]
-    allcategory = Category.objects.all()
-    tags = Tag.objects.all()
     # 分页处理
     page = request.GET.get("page")
     paginator = Paginator(list, 5)
@@ -56,9 +46,6 @@ def list(request, lid):
 # 内容
 def show(request, sid):
     show = Article.objects.get(id=sid)
-    allcategory = Category.objects.all()
-    tags = Tag.objects.all()
-    remen = Article.objects.filter(tui__id=2)[:6]
     # 随机推荐
     hot = Article.objects.all().order_by("?")[:10]
     previous_blog = Article.objects.filter(created_time__gt=show.created_time, category=show.category.id).first()
@@ -72,11 +59,8 @@ def show(request, sid):
 # 标签
 def tag(request, tag):
     list = Article.objects.filter(tags__name=tag)
-    remen = Article.objects.filter(tui__id=2)[:6]
-    allcategory = Category.objects.all()
     tname = Tag.objects.get(name=tag)
     page = request.GET.get("page")
-    tags = Tag.objects.all()
     paginator = Paginator(list, 5)
     try:
         list = paginator.page(page)
@@ -91,10 +75,8 @@ def tag(request, tag):
 def search(request):
     ss = request.GET.get("search")
     list = Article.objects.filter(title__icontains=ss)
-    remen = Article.objects.filter(tui__id=2)[:6]
     allcategory = Category.objects.all()
     page = request.GET.get("page")
-    tags = Tag.objects.all()
     paginator = Paginator(list, 10)
     try:
         list = paginator.page(page)
